e4ce055f4f33922e180a9209783a0366e6fdb1da
Ajay Ramsaran <ajayramsaran2012@gmail.com>
2024-11-11 16:50:58 -0500

    Update login.php

--- Habibi Carpooling/php/login.php
+++ Habibi Carpooling/php/login.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="habibiStyles.css"> <!-- Your stylesheet -->
 </head>
